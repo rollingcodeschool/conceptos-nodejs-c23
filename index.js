880b4b4958850d04737371213a4cf2cab5c0c31c
Emilse Arias <emilsearias@gmail.com>
2026-06-03 10:59:15 -0300

    agregamos los middlewares del proyecto

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // const express = require('express')
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
 const PORT = 3000
+
+// agregar los middlewares necesarios
+app.use(cors()) //permitir conexiones remotas
+app.use(express.json()) // permite interpretar los datos que lleguen en la solicitud en formato json
+// agrear el otro middleware morgan
 
 console.log('Este backend ya escucha 😁')
 console.log('Nuevo mensaje')
