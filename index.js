d0a93105b5c2221017ae1957329e64ab041d1d4c
Emilse Arias <emilsearias@gmail.com>
2026-06-08 11:03:40 -0300

    agregamos un archivo estatico de bienvenida

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = 3000;
@@ -9,7 +11,11 @@ const PORT = 3000;
 // agregar los middlewares necesarios
 app.use(cors()); //permitir conexiones remotas
 app.use(express.json()); // permite interpretar los datos que lleguen en la solicitud en formato json
-app.use(morgan('dev'))
+app.use(morgan("dev"));
+const __dirname = dirname(fileURLToPath(import.meta.url))
+console.log(__dirname + '/public')
+// cargar el siguiente archivo en forma estatica
+app.use(express.static(__dirname + '/public'))
 
 console.log("Este backend ya escucha 😁");
 console.log("Nuevo mensaje");
@@ -17,19 +23,17 @@ console.log("Nuevo mensaje");
 // endpoints
 // http://localhost:3000/api/saludo
 app.get("/api/saludo", (req, res) => {
-//   console.log("Me llego una solicitud nueva");
-  const comida= ['🍎', '🍏','🌭', '🧇', '🥯','🥐']
+  //   console.log("Me llego una solicitud nueva");
+  const comida = ["🍎", "🍏", "🌭", "🧇", "🥯", "🥐"];
   res.json({
     mensaje: "Conexion creada",
     saludo: "Bienvenido a nuestro backend",
-    comida
+    comida,
   });
 });
 
 app.get("/api/adios", (req, res) => {
-
-  res.json('hasta luego');
-  
+  res.json("hasta luego");
 });
 
 app.listen(PORT, () => {
